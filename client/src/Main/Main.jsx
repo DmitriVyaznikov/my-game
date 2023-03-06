8dd01df0b4aa1a7911c9910037c56bf5319ba985
Liquid1617 <ilignatow@gmail.com>
2023-03-07 03:01:02 +0800

    mainpage

--- client/src/Main/Main.jsx
+++ client/src/Main/Main.jsx
@@ -1,83 +1,28 @@
 import { light } from '@mui/material/styles/createPalette';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Row } from '../Row/Row';
-import styles from './main.module.css';
-
-const topics = [
-  {
-    id: 1,
-    name: 'History',
-    questions: [
-      {
-        id: 1,
-        questionText: 'Сколько будет 2+2?',
-        points: 200,
-        answered: false,
-      },
-      {
-        id: 2,
-        questionText: 'Сколько будет 3+2?',
-        points: 400,
-        answered: false,
-      },
-      {
-        id: 3,
-        questionText: 'Сколько будет 4+2?',
-        points: 600,
-        answered: true,
-      },
-      {
-        id: 4,
-        questionText: 'Сколько будет 5+2?',
-        points: 1000,
-        answered: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Math',
-    questions: [
-      {
-        id: 1,
-        questionText: 'Сколько будет 2+2?',
-        points: 200,
-        answered: true,
-      },
-      {
-        id: 2,
-        questionText: 'Сколько будет 3+2?',
-        points: 400,
-        answered: false,
-      },
-      {
-        id: 3,
-        questionText: 'Сколько будет 4+2?',
-        points: 600,
-        answered: true,
-      },
-      {
-        id: 4,
-        questionText: 'Сколько будет 5+2?',
-        points: 1000,
-        answered: false,
-      },
-    ],
-  },
-];
+import style from './main.module.css';
 
 function Main() {
+  const navigate = useNavigate();
+  const userName = JSON.parse(localStorage.getItem('user'));
   return (
-    <>
-      <div className={styles.mainBox}>
-        {topics.map((topic) => (
-          <Row
-            key={topic.id}
-            topic={topic}
-          />
-        ))}
-      </div>
-    </>
+    <div className={style.main}>
+      <h1>СВОЯ ИГРА</h1>
+      {userName ? (
+        <button
+          onClick={() => navigate('/')}
+          className={style.button}
+        >
+          {' '}
+          ИГРАТЬ
+          <span></span>
+        </button>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 
