30784d889486f25e928444461b19533ae6f8c0f0
Dmitriy Vyaznikov <dmitri.vyaznikov@gmail.com>
2023-03-07 14:53:37 +0700

    release fixed back sort

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -9,3 +9,8 @@ export const setUserInfo = (user) => ({
   type: ATypes.SET_USER,
   payload: { user: user },
 });
+
+export const setAnswers = (userId, gameId, correctQuestion, questionId) => ({
+  type: ATypes.SET_ANSWERS,
+  payload: { userId: userId, gameId: gameId, correctQuestion: correctQuestion, questionId: questionId },
+});
--- client/src/store/reducers.js
+++ client/src/store/reducers.js
@@ -3,33 +3,43 @@ import ATypes from "./types";
 const user = JSON.parse(localStorage.getItem("user")) || {};
 
 const initialState = {
-  topics: [],
-  user: {
-    userId: user.id,
-    username: user.login,
-    gameId: 1,
-  },
+    topics: [],
+    user: {
+        userId: user.id,
+        username: user.login,
+        gameId: 1,
+    },
+    answers: []
 };
 
 export const reducers = (state = initialState, action) => {
-  switch (action.type) {
-    case ATypes.SET_TOPICS:
-      return {
-        ...state,
-        topics: action.payload.topics,
-      };
-      case ATypes.SET_USER:
-        return {
-          ...state,
-          user: {
-            ...state.user,
-            username: action.payload.user.username,
-            userId: action.payload.user.userId,
-          },
-        };
+    switch (action.type) {
+        case ATypes.SET_TOPICS:
+            return {
+                ...state,
+                topics: action.payload.topics,
+            };
+        case ATypes.SET_USER:
+            return {
+                ...state,
+                user: {
+                    ...state.user,
+                    username: action.payload.user.username,
+                    userId: action.payload.user.userId,
+                },
+            };
+        case ATypes.SET_ANSWERS:
+            return {
+                ...state,
+                answers: {
+                    ...state.user,
+                    username: action.payload.user.username,
+                    userId: action.payload.user.userId,
+                },
+            };
 
 
-    default:
-      return state; // возвращаем зн-я в память. return - записывает новые значения в стор (храниться все в памяти память)
-  }
+        default:
+            return state; // возвращаем зн-я в память. return - записывает новые значения в стор (храниться все в памяти память)
+    }
 };
